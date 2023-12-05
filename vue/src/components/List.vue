baaf0bda0f80b6fbccfd41b540d76a849ae523a2
proxmetei <aleksei.korchagin200@mail.ru>
2023-12-06 00:15:35 +0300

    second commit

--- vue/src/components/List.vue
+++ vue/src/components/List.vue
@@ -6,61 +6,65 @@
         :key="book.id" 
         :style="cssProps"
       >
-        <div v-if="book.type == 'book'">
+        <template v-if="book.type == 'book'">
           <span>
-            {{ book.author + ' ' + book.initials + ' ' + book.title + ' : '
-              + book.typeBook + ' / ' + book.initials + book.author + '. – '
-              + book.editionNum + ' -е изд. – ' + book.city + ' : ' + book.publisher
-              + ', ' + book.year + '. – ' + book.pagesNum + ' с. – ISBN '
-              + book.isbn
+            {{ 
+              `${book.author} ${book.initials} ${book.title} : `
+              + `${book.typeBook} / ${book.initials} ${book.author}. – `
+              + `${book.editionNum ? `${book.editionNum} -е изд. – ` : ``}${book.city}`
+              + `${book.publisher ? ` : ${book.publisher}` : ``}`
+              + `, ${book.year}. – ${book.pagesNum}`
+              + ` с.${book.isbn ? ` – ISBN ${book.isbn}` : ``}`
             }}
           </span>
-        </div>
-        <div v-if="book.type == 'abstract'">
+        </template>
+        <template v-else-if="book.type == 'abstract'">
           <span>
-            {{ book.author + ' ' + book.initials + ' ' + book.title + ' : дис. ... '
-              + book.authorTitle + ' : ' + book.scientificSpecialty + ' / ' +  book.author
-              + ' ' + book.fullInitials + ' ; науч. рук. ' + book.supervisor
-              + ' ; ' + book.university + '. – ' + book.city +  ', ' + book.year
-              + '. – ' + book.pagesNum + ' с.'
+            {{ 
+              `${book.author} ${book.initials} ${book.title} : дис. ... `
+              + `${book.authorTitle} : ${book.scientificSpecialty} / ${book.author}`
+              + `  ${book.fullInitials}${book.supervisor ? ` ; науч. рук. ${book.supervisor}` : ``}`
+              + ` ; ${book.university}. – ${book.city}, ${book.year}`
+              + `. – ${book.pagesNum} с.`
             }}
           </span>
-        </div>
-        <div v-if="book.type == 'articleBook'">
+        </template>
+        <template v-else-if="book.type == 'articleBook'">
           <span>
-            {{ book.author + ' ' + book.initials + ' ' + book.title
-              + ' / ' + book.initials + book.author + ' // '
-              + book.originName + '. – ' + book.city + ', '
-              + book.year + '. – С. ' + book.pages
+            {{ 
+              `${book.author} ${book.initials} ${book.title}`
+              + ` / ${book.initials} ${book.author} // `
+              + `${book.originName}. – ${book.city}, `
+              + `${book.year}. – С. ${book.pages}`
             }}
           </span>
-        </div>
-        <div v-if="book.type == 'articleMagazine'">
+        </template>
+        <template v-else-if="book.type == 'articleMagazine'">
           <span>
-            {{ book.author + ' ' + book.initials + ' ' + book.title
-              + ' / ' + book.initials + book.author + ' // '
-              + book.originName + '. – ' + book.year + '. – №'
-              + book.magazineNum + '. – С. ' + book.pages
+            {{ 
+              `${book.author} ${book.initials} ${book.title}`
+              + ` / ${book.initials} ${book.author} // `
+              + `${book.originName}. – ${book.year}. – №`
+              + `${book.magazineNum}. – С. ${book.pages}`
             }}
           </span>
-        </div>
-        <div v-if="book.type == 'web'">
+        </template>
+        <template v-else-if="book.type == 'web'">
           <span>
-            {{ book.title + '. – URL: ' + book.url +
-              ' (дата обращения: ' + book.viewDate + ')'
+            {{ 
+              `${book.title}. – URL: ${book.url} (дата обращения: ${book.viewDate})`
             }}
           </span>
-        </div>
-        <div v-if="book.type == 'articleWeb'">
+        </template>
+        <template v-else-if="book.type == 'articleWeb'">
           <span>
-            {{  book.author + ' ' + book.initials + ' ' + book.title
-              + ' / ' + book.initials + book.author + ' // '
-              + book.originName + '. – ' + book.year 
-              + '. – URL: ' + book.url
-              + ' (дата обращения: ' + book.viewDate + ')'
+            {{  
+              `${book.author} ${book.initials} ${book.title}`
+              + `${book.initials}${book.author} // ${book.originName}. – ${book.year}`
+              + `. – URL: ${book.url} (дата обращения: ${book.viewDate})`
             }}
           </span>
-        </div>
+        </template>
         <el-button
             v-if="isEdit"
             type="danger"
